1ac1badb9edcd3a3038901339e4a1d7af5e19a46
lucid <155272051+dylndreams@users.noreply.github.com>
2025-01-01 17:23:25 -0500

    added eyecandy support
    all i did was rewrite some function names

--- examples/chenboot_triangle/main.c
+++ examples/chenboot_triangle/main.c
@@ -18,42 +18,11 @@ https://creativecommons.org/publicdomain/zero/1.0/
 #include <eyecandy.h>
 #include <psxdefs/gpu.h>
 #include <psxdefs/intc.h>
-#include <psxregs.h>
+#include <psxdefs/regs.h>
 
 volatile uint32_t vblank_counter = 0;
 volatile int out_vidy = 0;
 volatile int real_out_vidy = 0;
-
-void gpu_write_gp0_command(uint32_t v)
-{
-	int i;
-
-	for(i = 0; i < 1000; i+=1) {
-		if((PSXREG_GPU_GPUSTAT & (1<<26)) != 0) {
-			break;
-		}
-	}
-
-	PSXREG_GPU_GP0 = v;
-}
-
-void gpu_write_gp0_data(uint32_t v)
-{
-	int i;
-
-	for(i = 0; i < 1000; i+=1) {
-		if((PSXREG_GPU_GPUSTAT & (1<<28)) != 0) {
-			break;
-		}
-	}
-
-	PSXREG_GPU_GP0 = v;
-}
-
-void gpu_write_gp1(uint32_t v)
-{
-	PSXREG_GPU_GP1 = v;
-}
 
 chenboot_exception_frame_t *isr_main(chenboot_exception_frame_t *sp)
 {
@@ -70,7 +39,7 @@ chenboot_exception_frame_t *isr_main(chenboot_exception_frame_t *sp)
 	// vblank handler
 	if((iflags & INTC_VBLANK) != 0) {
 		vblank_counter += 1;
-		gpu_write_gp1(GP1_DISPLAY_START(0, out_vidy));
+		eyecandy_write_gp1(GP1_DISPLAY_START(0, out_vidy));
 		real_out_vidy = out_vidy;
 	}
 
@@ -100,25 +69,25 @@ int main(int argc, char *argv[])
 	);
 
 	// Reset GPU
-	gpu_write_gp1(GP1_RESET_GPU());
+	eyecandy_write_gp1(GP1_RESET_GPU());
 
 	// Set video mode
-	gpu_write_gp1(GP1_DISPLAY_MODE(
+	eyecandy_write_gp1(GP1_DISPLAY_MODE(
 		GPU_DISPMODE_W_320,
 		GPU_DISPMODE_H_240P,
 		GPU_DISPMODE_BPP_15,
 		GPU_DISPMODE_STD_PAL));
-	gpu_write_gp1(GP1_DISPLAY_RANGE_X(0x260, 0x260 + 320*8));
-	gpu_write_gp1(GP1_DISPLAY_RANGE_Y(0xA3 - 200/2, 0xA3 + 200/2));
-	gpu_write_gp1(GP1_DISPLAY_START(0, vidy));
+	eyecandy_write_gp1(GP1_DISPLAY_RANGE_X(0x260, 0x260 + 320*8));
+	eyecandy_write_gp1(GP1_DISPLAY_RANGE_Y(0xA3 - 200/2, 0xA3 + 200/2));
+	eyecandy_write_gp1(GP1_DISPLAY_START(0, vidy));
 
 	// Clear screen to colour
-	gpu_write_gp0_command(GP0_MEM_FILL(GPU_RGB8(0x00, 0x20, 0x40)));
-	gpu_write_gp0_data(GPU_BOX_OFFS(  0,   0+vidy));
-	gpu_write_gp0_data(GPU_BOX_SIZE(320, 200));
+	eyecandy_write_gp0_command(GP0_MEM_FILL(GPU_RGB8(0x00, 0x20, 0x40)));
+	eyecandy_write_gp0_data(GPU_BOX_OFFS(  0,   0+vidy));
+	eyecandy_write_gp0_data(GPU_BOX_SIZE(320, 200));
 
 	// Enable display
-	gpu_write_gp1(GP1_DISPLAY_ENABLE());
+	eyecandy_write_gp1(GP1_DISPLAY_ENABLE());
 
 	// Reset vblank counter
 	vblank_counter = 0;
@@ -135,28 +104,28 @@ int main(int argc, char *argv[])
 		vidy = 200-vidy;
 
 		// Set rendering attribs
-		gpu_write_gp0_command(GP0_ATTR_TEXPAGE(
+		eyecandy_write_gp0_command(GP0_ATTR_TEXPAGE(
 			0,
 			0,
 			GPU_TEXPAGE_BLEND_HALF,
 			GPU_TEXPAGE_TEXBPP_4,
 			GPU_TEXPAGE_DITHER | GPU_TEXPAGE_DRAWTODISPLAY));
-		gpu_write_gp0_command(GP0_ATTR_TEXWINDOW(0,0,0,0));
-		gpu_write_gp0_command(GP0_ATTR_DRAW_RANGE_MIN(0, 0+vidy));
-		gpu_write_gp0_command(GP0_ATTR_DRAW_RANGE_MAX(320-1, (200-1)+vidy));
-		gpu_write_gp0_command(GP0_ATTR_DRAW_OFFSET(320/2, (200/2)+vidy));
-		gpu_write_gp0_command(GP0_ATTR_MASKBIT(0));
+		eyecandy_write_gp0_command(GP0_ATTR_TEXWINDOW(0,0,0,0));
+		eyecandy_write_gp0_command(GP0_ATTR_DRAW_RANGE_MIN(0, 0+vidy));
+		eyecandy_write_gp0_command(GP0_ATTR_DRAW_RANGE_MAX(320-1, (200-1)+vidy));
+		eyecandy_write_gp0_command(GP0_ATTR_DRAW_OFFSET(320/2, (200/2)+vidy));
+		eyecandy_write_gp0_command(GP0_ATTR_MASKBIT(0));
 
 		// Clear screen to colour
-		gpu_write_gp0_command(GP0_MEM_FILL(GPU_RGB8(0x00, 0x20, 0x40)));
-		gpu_write_gp0_data(GPU_BOX_OFFS(  0,   0+vidy));
-		gpu_write_gp0_data(GPU_BOX_SIZE(320, 200));
+		eyecandy_write_gp0_command(GP0_MEM_FILL(GPU_RGB8(0x00, 0x20, 0x40)));
+		eyecandy_write_gp0_data(GPU_BOX_OFFS(  0,   0+vidy));
+		eyecandy_write_gp0_data(GPU_BOX_SIZE(320, 200));
 
 		// Draw a triangle
-		gpu_write_gp0_command(GP0_TRI_FLAT(GPU_RGB8(0xFF, 0x80, 0x00)));
-		gpu_write_gp0_data(GPU_VERTEX(( 60)+x, (-50)+y));
-		gpu_write_gp0_data(GPU_VERTEX((  0)+x, ( 70)+y));
-		gpu_write_gp0_data(GPU_VERTEX((-60)+x, (-50)+y));
+		eyecandy_write_gp0_command(GP0_TRI_FLAT(GPU_RGB8(0xFF, 0x80, 0x00)));
+		eyecandy_write_gp0_data(GPU_VERTEX(( 60)+x, (-50)+y));
+		eyecandy_write_gp0_data(GPU_VERTEX((  0)+x, ( 70)+y));
+		eyecandy_write_gp0_data(GPU_VERTEX((-60)+x, (-50)+y));
 
 		//
 		// PHYSICS
